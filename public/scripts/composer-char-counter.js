05d468fe33028ce0f18a37269f9f3543bd7d2063
Aleksandar Dmitrovic <aleksandar.dmitrovic@gmail.com>
2020-10-06 15:20:11 +0000

    Complete Implementation of Character Counter
    Using jQuery I updated the tweet character counter to indicate remaining chars turning red with negative numbers

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,5 +1,25 @@
+// const e = require("express");
+
+
 $(document).ready(function() {
-  $("#tweet-text").on("keypress", function(){
-    console.log(this)
+  $("#tweet-text").on("input", function(e){
+    let tweetChar = $(this).val().length
+    let charCount = 140 - tweetChar;
+
+    let thisObj = $(this);
+    console.log(thisObj)
+
+    // let charCountWithThis = $(this)[0].nextElementSibling.children[1]//---Accessing the output html tag with name counter
+    // charCountWithThis = $(charCountWithThis)[0].innerText;          //---with the method compass wants. I choose to make an
+    // console.log('charCountWithThis :', charCountWithThis);          //-- id for this output instead
+   
+    if(charCount >= 0) {
+      $("#char-count" ).text(charCount);
+    } else {
+      $("#char-count" ).css("color", "red");
+      $("#char-count" ).text(charCount);
+    }
+    
+
   });
 });
