45c50e9009cd6ad0bb4b79dce1121277a98ece36
Aleksandar Dmitrovic <aleksandar.dmitrovic@gmail.com>
2020-10-08 19:53:59 +0000

    Lint and clean code

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,16 +3,16 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-$(document).ready(function () {
+$(document).ready(function() {
 
   //Escape function to prevent XSS
-  const escape = function (str) {
+  const escape = function(str) {
     let div = document.createElement('div');
     div.appendChild(document.createTextNode(str));
     return div.innerHTML;
   };
 
-  const createTweetElement = function (tweet) {
+  const createTweetElement = function(tweet) {
     let $tweet = `
     <article class="tweet">
     <header>
@@ -30,57 +30,56 @@ $(document).ready(function () {
     <button><i class="fas fa-heart"></i></button>
     </div>
     </footer>
-    </article>`
+    </article>`;
     return $tweet;
   };
 
-  const renderTweets = function (tweets) {
+  const renderTweets = function(tweets) {
     tweets.forEach(tweet => {
       $('#tweets-container').prepend(createTweetElement(tweet));
     });
   };
 
   // Ajax post request -shorthand with promise
-
-  $('form').on('submit', function (event) {
-    event.preventDefault()
+  $('form').on('submit', function(event) {
+    event.preventDefault();
     let $tweet = $(this).serialize();
 
-    //validation
+    //Validation of input tweet
     $input = $('#tweet-text').val();
     if (!$input) {
-      $("#error-message").text("Please write a tweet")
+      $("#error-message").text("Please write a tweet");
       $('.validation').slideDown();
     } else if ($input.length > 140) {
-      $("#error-message").text("Tweet too long. Please keep it short and sweet")
+      $("#error-message").text("Tweet too long. Please keep it short and sweet");
       $('.validation').slideDown();
     } else {
       $('.validation').slideUp();
       $.ajax('/tweets', { method: 'POST', data: $tweet })
-        .then(function (newTweet) {
+        .then(() => {
           loadtweets();
-          $('#tweet-text').val('')
-          $("#char-count").text(140)
+          $('#tweet-text').val('');
+          $("#char-count").text(140);
         })
         .catch((error) => {
           console.log(error);
-        })
+        });
 
     }
   });
 
   // Ajax get request rendering tweets to home page
-  const loadtweets = function () {
+  const loadtweets = function() {
     $.ajax('/tweets', { method: 'GET' })
       .then(renderTweets)
-      .catch( (error) => {
+      .catch((error) => {
         console.log(error);
-      })
+      });
   };
 
   loadtweets();
   
-  //Nav Toggle Button for the tweet form 
+  //Nav Toggle Button for the tweet form
   $("#display-tweet-form").on("click", () => {
     $('.new-tweet').slideToggle();
     $('#tweet-text').focus();
@@ -90,7 +89,7 @@ $(document).ready(function () {
   //Exectuted on scroll
   $(window).on("scroll", function() {
     $('#scroll-up').show();
-    $("#display-tweet-form").hide()
+    $("#display-tweet-form").hide();
   });
   
   //Scroll to Top Button
@@ -111,19 +110,19 @@ $(document).ready(function () {
 
 //-----------------------------------------------------------------------------
 
-  // // AJAX REQUEST LONG FORM --- For my own notes to learn from ---
-  // $('form').on('submit', function (event) {
-  //   event.preventDefault();
-  //   const $tweet = $(this).serialize();
-  //   $.ajax({
-  //     url: '/tweets',
-  //     method: 'POST',
-  //     data: $tweet,
-  //     success: function(newTweet) {
-  //     console.log('Success', newTweet)
-  //     },
-  //     error: function(error) {
-  //       console.log(error);
-  //     }
-  //   });
-  // });
+// // AJAX REQUEST LONG FORM --- For my own notes to learn from ---
+// $('form').on('submit', function (event) {
+//   event.preventDefault();
+//   const $tweet = $(this).serialize();
+//   $.ajax({
+//     url: '/tweets',
+//     method: 'POST',
+//     data: $tweet,
+//     success: function(newTweet) {
+//     console.log('Success', newTweet)
+//     },
+//     error: function(error) {
+//       console.log(error);
+//     }
+//   });
+// });
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,23 +1,21 @@
-// const e = require("express");
+//Tweet Character Count Tracker
 
-
-$(document).ready(function () {
-  $("#tweet-text").on("input", function (e) {
-    let tweetChar = $(this).val().length
-    let charCount = 140 - tweetChar;
-
-    // let charCountWithThis = $(this)[0].nextElementSibling.children[1]//---Accessing the output html tag with name counter
-    // charCountWithThis = $(charCountWithThis)[0].innerText;          //---with the method compass wants. I choose to make an
-    // console.log('charCountWithThis :', charCountWithThis);          //-- id for this output instead
-
-    if (charCount >= 0) {
-      $("#char-count").css("color", "black");
-      $("#char-count").text(charCount);
-    } else if (charCount < 0) {
-      $("#char-count").css("color", "red");
-      $("#char-count").text(charCount);
-    }
-
-
+const charCountTracker = () => {
+  $(document).ready(function() {
+    $("#tweet-text").on("input", function() {
+      let tweetChar = $(this).val().length;
+      let charCount = 140 - tweetChar;
+  
+      if (charCount >= 0) {
+        $("#char-count").css("color", "black");
+        $("#char-count").text(charCount);
+      } else if (charCount < 0) {
+        $("#char-count").css("color", "red");
+        $("#char-count").text(charCount);
+      }
+  
+    });
   });
-});
+};
+
+charCountTracker();
